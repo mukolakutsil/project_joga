7341b5420375c6f8983126b500494cf09469e7f8
Mukola_Kutsil <mukola_kutsil@mukola.kutsil.Inspiron>
2019-11-01 00:05:24 +0200

    Timer_js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+
 window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
+
+    //   Tabb
+
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
@@ -34,5 +38,48 @@ window.addEventListener('DOMContentLoaded', function() {
         }
 
     });
+
+
+    //  Timer
+
+ let deadLine = '2019-11-10';
+
+ function getTimeRemaining (endtime) {
+     let t = Date.parse(endtime) - Date.parse(new Date()),
+     seconds = Math.floor((t/1000) % 60),
+     minutes = Math.floor((t/1000/60) % 60),
+     hours = Math.floor((t/(1000*60*60)));
+    
+     return {
+         'total' : t,
+         'hours' : hours,
+         'minutes' : minutes,
+         'seconds' : seconds
+      };
+    } 
+
+ function setClock(id, endtime) {
+      let timer = document.getElementById(id),
+          hours = document.querySelector('.hours'),
+          minutes = document.querySelector('.minutes'),
+          seconds = document.querySelector('.seconds'),
+          timeInterval = setInterval(updateClock, 1000);
+
+      function updateClock() {
+          let t = getTimeRemaining(endtime);
+          hours.textContent = t.hours;
+          minutes.textContent = t.minutes;
+          seconds.textContent = t.seconds;
+
+          if (t.total <= 0) {
+              clearInterval(timeInterval);
+          }
+      }    
+ };
+
+ setClock('timer', deadLine );
+
 });
       
+
+
