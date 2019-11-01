ae8594ab2422a040c8e2f8750de5a0dee715b4bf
Mukola_Kutsil <mukola_kutsil@mukola.kutsil.Inspiron>
2019-11-01 16:07:51 +0200

    modifide_Modal

--- script.js
+++ script.js
@@ -83,24 +83,25 @@ window.addEventListener('DOMContentLoaded', function() {
 
 //    Modal
 
- let more = document.querySelector('.more'),
-     overlay = document.querySelector('.overlay'),
-     close = document.querySelector('.popup-close'),
-     desBtn = document.querySelector('.description-btn');
+  let more = document.querySelector('.more'),
+      overlay = document.querySelector('.overlay'),
+      close = document.querySelector('.popup-close');
 
-     desBtn.addEventListener('click', function() {
-         overlay.style.display = 'block';
-         this.classList.add('more-splash');
-         document.body.style.overflow = 'hidden';
-     });
+  more.addEventListener('click', function() {
+      overlay.style.display = 'block';
+      this.classList.add('more-splash');
+      document.body.style.overflow = 'hidden';
+});
 
-      close.addEventListener('click', function() {
-         overlay.style.display = 'none';
-         more.classList.remove('more-splash');
-         document.body.style.overflow = '';
-     });
+   close.addEventListener('click', function() {
+      overlay.style.display = 'none';
+      more.classList.remove('more-splash');
+      document.body.style.overflow = '';
+  });
 
 });
+
+
       
 
 
